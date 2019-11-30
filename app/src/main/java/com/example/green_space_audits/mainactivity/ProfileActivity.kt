74afefb7698562fdbb632b149c3ac398972bd367
Divya Gupta <dgupta@Divyas-MacBook-Pro-2.local>
2019-11-30 00:37:31 -0500

    saving profile changes

--- app/src/main/java/com/example/green_space_audits/mainactivity/ProfileActivity.kt
+++ app/src/main/java/com/example/green_space_audits/mainactivity/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.example.green_space_audits.mainactivity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,48 +12,63 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
-
-
-
+import org.w3c.dom.Text
 
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var nameHolder: TextView
+    private lateinit var emailHolder: TextView
     private lateinit var pointsHolder: TextView
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDatabase: FirebaseDatabase
     private lateinit var mDatabaseReference: DatabaseReference
-
+    private var currName: String? = null
+    private var currEmail: String? = null
+    private var currPoints: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         nameHolder = findViewById<View>(R.id.profile_name) as TextView
+        emailHolder = findViewById<View>(R.id.profile_email) as TextView
         pointsHolder = findViewById<View>(R.id.profile_points) as TextView
         mAuth = FirebaseAuth.getInstance()
 
         mDatabase = FirebaseDatabase.getInstance()
+//        mDatabaseReference = mDatabase!!.reference!!
         mDatabaseReference = mDatabase!!.reference!!.child("Users")
     }
 
     override fun onStart() {
         super.onStart()
-        var id = mAuth!!.currentUser!!.uid
+        val id = mAuth!!.currentUser!!.uid
+//        val usr = mDatabaseReference.child("Users").child(id)
 
-        var usr = mDatabaseReference.child(id)
+        // Attach a listener to read the data
+        mDatabaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val currUser = dataSnapshot.child(id).getValue(User::class.java)
+                currName = currUser!!.userName
+                currEmail = currUser!!.userEmail
+                currPoints = currUser!!.uPoints.toString()
 
-//        // Attach a listener to read the data
-//        mDatabaseReference.addValueEventListener(object : ValueEventListener {
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                val currUser = dataSnapshot.getValue(User::class.java)
-//                if ()
-//            }
-//
-//            override fun onCancelled(databaseError: DatabaseError) {
-//                println("The read failed: " + databaseError.code)
-//            }
-//        })
+//                currName = dataSnapshot.child("userName").getValue(String::class.java)
+//                currEmail = dataSnapshot.child("userEmail").getValue(String::class.java)
+//                currPoints = dataSnapshot.child("uPoints").getValue(String::class.java)
+                Log.i("profile", currName!!.toString() + " " + currEmail!!.toString() + " " + currPoints!!.toString())
+
+                // set values in textviews
+                nameHolder.text = currName
+                emailHolder.text = currEmail
+                pointsHolder.text = currPoints
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("The read failed: " + databaseError.code)
+            }
+        })
+
     }
 }
