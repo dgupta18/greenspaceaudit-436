e66351aa58e89cc39a59d831f7c1a036fb00f13e
Divya Gupta <dgupta@wireless-10-104-66-236.umd.edu>
2019-12-03 16:20:30 -0500

    adding login fix and updating website

--- app/src/main/java/com/example/green_space_audits/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/green_space_audits/mainactivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.green_space_audits.mainactivity
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,9 +13,18 @@ class MainActivity : AppCompatActivity() {
     internal var registerBtn: Button? = null
     internal var loginBtn: Button? = null
     internal var Splash_time = 4000L
+    private lateinit var pref: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        pref = getSharedPreferences("adminKey", Context.MODE_PRIVATE)
+        if (pref.all.isNotEmpty()) {
+            val enter = Intent(this@MainActivity, MapsActivity::class.java)
+            startActivity(enter)
+            overridePendingTransition(0, 0)
+        }
         setContentView(R.layout.activity_main)
 
 //        Handler().postDelayed({
